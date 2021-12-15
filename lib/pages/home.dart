d38a65c38b235561d1328bf954c9cfbe63a36689
Allan Franco <allan.franco@faci.ly>
2021-12-14 23:01:19 -0300

    add form and task list

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -7,29 +9,105 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var count = 0;
+  final newTaskController = TextEditingController();
+  var list = [];
 
-  void increment() {
-    count++;
-    setState(() {});
-    print(count);
+  void showSnackBar() {
+    final snackBar = SnackBar(
+      content: const Text('Digite o nome da tarefa!'),
+      action: SnackBarAction(
+        label: 'OK',
+        onPressed: () {},
+      ),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void addTask() {
+    if (newTaskController.text.isNotEmpty) {
+      list.add(newTaskController.text);
+      newTaskController.text = "";
+      setState(() {});
+    } else {
+      showSnackBar();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "COUNTER\n$count",
-          textAlign: TextAlign.center,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          increment();
-        },
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 6,
+                  child: TextField(
+                    controller: newTaskController,
+                    decoration: InputDecoration(
+                      label: Text("Nome da Tarefa"),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 10),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(20, 65),
+                      ),
+                      onPressed: () {
+                        addTask();
+                      },
+                      child: Text(
+                        'Adicionar',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Column(
+                children: [for (var item in list) ListItem(item)],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+}
+
+Widget ListItem(String title) {
+  return Align(
+    alignment: Alignment.centerLeft,
+    child: Column(
+      children: [
+        Container(
+          width: 1000,
+          margin: EdgeInsets.only(bottom: 5),
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(width: 1, color: Colors.deepPurple),
+            ),
+          ),
+          child: Text(
+            title,
+            style: TextStyle(fontSize: 16),
+            textAlign: TextAlign.left,
+          ),
+        )
+      ],
+    ),
+  );
 }
